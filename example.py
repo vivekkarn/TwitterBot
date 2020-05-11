36d2e3a92e18a8dee1f47a6425e06ca0533555bf
Vivek Karn <mailervivek@gmail.com>
2020-05-12 00:25:08 +0545

    Improved example

--- example.py
+++ example.py
@@ -10,7 +10,14 @@ number_of_iterations = 100
 # Your query to search and like the terms
 query = 'doctors'
 
+# The SECRECT_KEY, SECRET_TOKEN, ACCESS_KEY, ACESS_TOKEN
+# is available at https://developer.twitter.com/en/apps
+
 bot = TwitterBot(number_of_iterations, SECRECT_KEY,
                  SECRET_TOKEN, ACCESS_KEY, ACESS_TOKEN)
+
+# Followback all your followers if you are not already following them
 bot.follow_followers()
+
+# Like 100 tweets from the provided query
 bot.like_tweets(query)
